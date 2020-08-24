90332acbfd49929a90bf3024a64eed23531161d8
Fayyaz-Imran <fayazimran.work@gmail.com>
2020-08-24 17:54:28 +0800

    Added basic files setup

---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _buildTitle(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitle() => Text(
+    "Safe Area",
+    textAlign: TextAlign.center,
+  );
+
+}
